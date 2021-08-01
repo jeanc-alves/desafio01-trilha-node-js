7046f60ed48ce34df858c8fed702ddab8a4c1e3a
Jean Alves <jean.alves@inovan.do>
2021-08-01 20:21:29 -0300

    chore(categorias): refactory list categories

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -6,16 +6,14 @@ interface IRequest {
 }
 
 class CreateCategoryUseCase {
-    constructor(private categoriesRepository: ICategoriesRepository) {
-
-    }
+    constructor(private categoriesRepository: ICategoriesRepository) { }
 
     execute({ name, description }: IRequest): void {
         const categoryAlreadyExist = this.categoriesRepository.findByName(name)
+
         if (categoryAlreadyExist) {
             throw new Error("Category Already Exists")
         }
-
         this.categoriesRepository.create({ name, description })
     }
 }
