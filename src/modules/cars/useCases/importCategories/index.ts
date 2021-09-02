f3b2f2f80df53ec1aa98aa81b12cff61796310e8
Jean Alves <jean.alves@inovan.do>
2021-09-02 09:25:09 -0300

    feat(Tsrynge): config Injection Dependence

--- src/modules/cars/useCases/importCategories/index.ts
+++
@@ -1,9 +0,0 @@
-import { ImportCategoryController } from "./ImportCategoryController";
-import { ImportCategoryUseCase } from "../importCategories/ImportCategoryUseCase";
-import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
-
-const categoriesRepository = null
-const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository)
-const importCategoryController = new ImportCategoryController(importCategoryUseCase)
-
-export { importCategoryController }
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express'
-import { createSpecificationController } from '../modules/cars/useCases/createSpecifications'
+import { CreateSpecificationController } from '../modules/cars/useCases/createSpecifications/CreateSpecificationController'
 
 const specificationRoutes = Router()
 
+const createSpecificationController = new CreateSpecificationController()
 
-specificationRoutes.post("/", (request, response) => {
-    return createSpecificationController.handle(request, response)
-})
+specificationRoutes.post("/", createSpecificationController.handle)
 
 export { specificationRoutes }
