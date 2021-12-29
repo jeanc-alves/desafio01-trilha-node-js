6f8b07a17a062436f346e7cb784c834686bb3904
Jean Alves <jean.alves@inovan.do>
2021-12-29 07:05:26 -0300

    chore(email): create template email forgot password

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -1,5 +1,7 @@
 import { inject, injectable } from "tsyringe";
 import { v4 as uuidV4 } from "uuid";
+import { resolve } from "path";
+
 import { IDateProvider } from "../../../../shared/container/providers/DateProvider/IDateProvider";
 import { IMailProvider } from "../../../../shared/container/providers/MailProvider/IMailProvider";
 
@@ -22,6 +24,15 @@ class SendForgotPasswordMailUseCase {
   async execute(email: string): Promise<void> {
     const user = await this.usersRepository.findByEmail(email);
 
+    const templatePath = resolve(
+      __dirname,
+      "..",
+      "..",
+      "views",
+      "emails",
+      "forgotPassword.hbs"
+    );
+
     if (!user) {
       throw new AppError("User Does not exists");
     }
@@ -35,10 +46,16 @@ class SendForgotPasswordMailUseCase {
       expires_date,
     });
 
+    const variables = {
+      name: user.name,
+      link: `${process.env.FORGOT_PASSWORD_URL}${token}`,
+    };
+
     await this.mailProvider.sendMail(
       email,
-      "Recuperação de senha",
-      `O link para o reset é ${token}`
+      "Recuperação de Senha",
+      variables,
+      templatePath
     );
   }
 }
